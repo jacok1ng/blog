7cb6e158678ef27f87c0683020007ac248fc1f16
Eryk Dąbek <eryk.dabek@euvic.pl>
2022-11-28 19:46:13 +0100

    Add showInfoBox func

--- cards.php
+++ cards.php
@@ -110,37 +110,56 @@
   function displayButtons(){
     $stage = isset($_GET['state']) ? $_GET['state'] : $_SESSION['CARD_STATUS'];
     if($_SESSION["GRACZ_WYNIK"] > 21){
-      echo '<div class="lose info-box">';
-      echo '<p>Przegrałeś! Spróbuj ponownie!</p>';
-      echo '</div>';
+      showInfoBox('lose');
     }
     else{
       if($stage == '3'){
         if($_SESSION["KRUPIER_WYNIK"] > 21){
-          echo '<div class="win info-box">';
-          echo '<p>Wygrałeś! Gratulacje!</p>';
-          echo '</div>';
+          showInfoBox('win');
         }
         else if($_SESSION["KRUPIER_WYNIK"] == $_SESSION["GRACZ_WYNIK"]){
-          echo '<div class="draw info-box">';
-          echo '<p>Remis!</p>';
-          echo '</div>';
+          showInfoBox('draw');
         }
         else if($_SESSION["KRUPIER_WYNIK"] <= 21 && ($_SESSION["KRUPIER_WYNIK"] > $_SESSION["GRACZ_WYNIK"]))
         {
-          echo '<div class="lose info-box">';
-          echo '<p>Przegrałeś! Spróbuj ponownie!</p>';
-          echo '</div>';
+          showInfoBox('lose');
         }
         else{
-          echo '<div class="win info-box">';
-          echo '<p>Wygrałeś! Gratulacje!</p>';
-          echo '</div>';
+          showInfoBox('win');
         }
       }
       else{
         echo '<a href="./cards.php?state=2"><button class="styled-button">Następna karta</button></a>';
         echo '<a href="./cards.php?state=3"><button class="styled-button">Stop</button></a>';
+      }
+    }
+  }
+
+  function showInfoBox($type){
+    switch($type){
+      case 'draw':{
+        echo '<div class="draw info-box">';
+        echo '<p>Remis!</p>';
+        echo '</div>';
+        break;
+      }
+      case 'win':{
+        echo '<div class="win info-box">';
+        echo '<p>Wygrałeś! Gratulacje!</p>';
+        echo '</div>';
+        break;
+      }
+      case 'lose':{
+        echo '<div class="lose info-box">';
+        echo '<p>Przegrałeś! Spróbuj ponownie!</p>';
+        echo '</div>';
+        break;
+      }
+      default: {
+        echo '<div class="lose info-box">';
+        echo '<p>Przegrałeś! Spróbuj ponownie!</p>';
+        echo '</div>';
+        break;
       }
     }
   }
